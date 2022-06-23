69f2cd882a34d77cc35877d99241066d7041958c
Artem Alekseev <lexx73652@gmail.com>
2022-06-23 16:50:36 +0400

    Updated 8.5 class version

--- db/restoredb.sh
+++ db/restoredb.sh
@@ -5,10 +5,9 @@ CREATE DATABASE [Authentication];
 CREATE DATABASE [CabalCash];
 CREATE DATABASE [CabalGuild];
 CREATE DATABASE [EventData];
-CREATE DATABASE [NetCafeBilling];"
-
-touch /var/opt/mssql/data/testing-server01.ldf
-touch /var/opt/mssql/data/testing-server01.mdf
+CREATE DATABASE [NetCafeBilling];
+CREATE DATABASE [AnderSystems];
+CREATE DATABASE [Server01];"
 
 chown mssql /var/opt/mssql/data
 
@@ -18,4 +17,5 @@ RESTORE DATABASE [CabalCash] FROM DISK = N'/var/opt/mssql/backup/CabalCash.bak' 
 RESTORE DATABASE [CabalGuild] FROM DISK = N'/var/opt/mssql/backup/CabalGuild.bak' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 5;
 RESTORE DATABASE [EventData] FROM DISK = N'/var/opt/mssql/backup/EventData.bak' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 5;
 RESTORE DATABASE [NetCafeBilling] FROM DISK = N'/var/opt/mssql/backup/NetCafeBilling.bak' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 5;
-RESTORE DATABASE [Server01] FROM DISK = N'/var/opt/mssql/backup/Server01.bak' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 5;"
+RESTORE DATABASE [Server01] FROM  DISK = N'/var/opt/mssql/backup/Server01.bak' WITH  FILE = 1,  MOVE N'SERVER02' TO N'/var/opt/mssql/data/Server01.mdf',  MOVE N'SERVER02_log' TO N'/var/opt/mssql/data/Server01_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 5;
+RESTORE DATABASE [AnderSystems] FROM  DISK = N'/var/opt/mssql/backup/AnderSystems.bak' WITH  FILE = 1,  MOVE N'AnderSystems' TO N'/var/opt/mssql/data/AnderSystems.mdf',  MOVE N'AnderSystems_log' TO N'/var/opt/mssql/data/AnderSystems_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 5;"
